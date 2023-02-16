e9d4f7011fc12821edcef8de5cbdd599c05082cb
qgzm <jhb_j@qq.com>
2023-02-16 18:28:46 +0800

    commit

--- demo/demo0/跑步锻炼.py
+++ demo/demo0/跑步锻炼.py
@@ -1,2 +1,15 @@
 # 使用者：姜海波
 # 创建时间：2023/1/23  21:06
+import datetime
+start=datetime.date(2000,1,1)
+end=datetime.date(2020,10,1)
+days=datetime.timedelta(days=1)
+ans=0
+while end>=start:
+    if start.day==1 or start.weekday()==0:
+        ans+=2
+    else:
+        ans+=1
+    start+=days
+
+print(ans)
